045e21839e933c6915b3180d5fb51707a53f9917
Minyoung <alsendrha@naver.com>
2024-09-06 16:58:42 +0900

    디테일 페이지 인디케이터 위치 변경

--- src/screen/Detail.tsx
+++ src/screen/Detail.tsx
@@ -28,7 +28,11 @@ const Detail = ({route}: DetailProps) => {
   const {width} = useWindowDimensions();
 
   if (isLoading || imagesLoading)
-    return <ActivityIndicator size="large" color="#0000ff" />;
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
   return (
     <ScrollView style={styles.bacColor}>
       {data.map((item: DetailItemType) => (
